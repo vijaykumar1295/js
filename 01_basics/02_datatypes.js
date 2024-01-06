ce4518d2fc396a0a0e087f9941f39ab7be80f556
vijaykumar1295 <77140435+vijaykumar1295@users.noreply.github.com>
2024-01-06 17:24:20 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"   // treat all JS code as newer version
+
+// alert( 3 + 3 )  // we are using nodejs,not browser
+
+// https://tc39.es/ecma262/    * javascript standard
+
+let name = "vijay"   // String
+let age  = 28   // Number
+let isLoggedIn = true  // boolean
+
+
+// number = 2 to power of 53
+// bigint 
+// string => ""
+// boolean =>true/false
+// null => standalone value
+// undefined => 
+// symbol  => unique
+
+
+// object
+
+console.log(typeof null)
+console.log(typeof undefined)
